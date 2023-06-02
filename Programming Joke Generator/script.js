826f675538428d05c31acccbf1e0947191ac3143
FARDIN98 <fardinahmed66@gmail.com>
2023-06-02 23:41:25 +0600

    updated

--- Programming Joke Generator/script.js
+++ Programming Joke Generator/script.js
@@ -1,15 +1,23 @@
-const jokeContainer =  document.getElementById('joke');
+// Get the joke container element from the DOM using its ID
+const jokeContainer = document.getElementById('joke');
+
+// Get the button element from the DOM using its ID
 const btn = document.getElementById('btn');
+
+// Define the URL for the API that provides programming jokes, with certain blacklist flags
 const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
-
+// Define a function to fetch a joke from the API and display it in the joke container
 let getJoke = () => {
-    fetch(url)
-    .then(data => data.json())
-    .then(item => {
-        jokeContainer.innerText = `${item.joke}`
+    fetch(url) // Fetch the joke data from the API
+    .then(data => data.json()) // Convert the response data to JSON
+    .then(item => { // Extract the joke from the JSON response
+        jokeContainer.innerText = `${item.joke}`; // Display the joke in the joke container
     });
 }
 
-btn.addEventListener("click",getJoke);
+// Add a click event listener to the button, which triggers the getJoke function when clicked
+btn.addEventListener("click", getJoke);
+
+// Call the getJoke function to display a joke when the page loads
 getJoke();
